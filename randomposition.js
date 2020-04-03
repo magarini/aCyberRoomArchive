628b3ccd07a0c11e85c18bcebf6216ee607b54ee
magarini <56253774+magarini@users.noreply.github.com>
2020-04-03 05:24:37 +0300

    Update randomposition.js

--- randomposition.js
+++ randomposition.js
@@ -1,12 +1,13 @@
 function setup() {
-    canvas = createCanvas(innerWidth, innerHeight);
-  canvas.position(0, 0);
+    canvas = createCanvas(windowWidth, windowHeight);
+    canvas.parent('multidraggable');
+//   canvas.position(0, 0);
   canvas.style('z-index','-1');
-
+canvas.position(0,50);
     let buttons = selectAll('.imgdrag');
 for (let i = 0; i < buttons.length; i++) {
     
-    buttons[i].position(random(width-100), random(height-100),'relative');
+    buttons[i].position(random(width-200), random(height-200));
     console.log(buttons[i]);
   }
 }
